5f6785b15a88848cc4631a7a6e1a0839715d5156
Duong Thuyet <thuyetdoras.3@gmail.com>
2024-09-28 11:38:59 +0700

    first commit

---
+++ HelloWorld.java
@@ -0,0 +1,12 @@
+package week1;
+
+/**
+ * HelloWorld
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Xin chao \n cac ban!");
+        System.out.println("Hello \tWorld!");
+    }
+}
