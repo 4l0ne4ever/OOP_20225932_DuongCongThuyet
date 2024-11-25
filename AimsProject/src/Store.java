0ccfeaa69b780c129993780218d539b910907689
4l0ne4ever <thuyetdoras.3@gmail.com>
2024-11-25 16:37:56 +0700

    add store and test

---
+++ AimsProject/src/Store.java
@@ -0,0 +1,49 @@
+package AimsProject.src;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Store {
+    private List<DigitalVideoDisc> itemsInStore;
+
+    public Store() {
+        itemsInStore = new ArrayList<>();
+    }
+
+    public void addDvd(DigitalVideoDisc dvd) {
+        itemsInStore.add(dvd);
+
+    }
+
+    public void removeDVD(DigitalVideoDisc dvd) {
+        boolean isRemoved = itemsInStore.remove(dvd);
+        if (removed) {
+            System.out.println(dvd.getTitle() + " has been removed from the store.");
+        } else {
+            System.out.println(dvd.getTitle() + " is not found in the store.");
+        }
+    }
+
+    public void print() {
+        for (int i = 0; i < itemsInStore.size(); i++) {
+            System.out.println(i + 1 + ". " + itemsInStore.get(i));
+        }
+    }
+
+    public int getTotalItems() {
+        return itemsInStore.size();
+    }
+
+    public DigitalVideoDisc findDVDByTitle(String title) {
+        for (DigitalVideoDisc dvd : itemsInStore) {
+            if (dvd.getTitle().equalsIgnoreCase(title)) {
+                return dvd;
+            }
+        }
+        return null;
+    }
+
+    public List<DigitalVideoDisc> getItemsInStore() {
+        return new ArrayList<>(itemsInStore);
+    }
+}
---
+++ AimsProject/src/StoreTest.java
@@ -0,0 +1,18 @@
+package AimsProject.src;
+
+public class StoreTest {
+    public static void main(String[] args) {
+        Store store = new Store();
+        DigitalVideoDisc dvd1 = new DigitalVideoDisc("Lion King", "Animation", "Roger Allers", 87, 19.95f);
+        store.addDvd(dvd1);
+
+        DigitalVideoDisc dvd2 = new DigitalVideoDisc("Star War", "Sci-fic", "Geogre Lucas", 87, 22.42f);
+        store.addDvd(dvd2);
+
+        DigitalVideoDisc dvd3 = new DigitalVideoDisc("Alien", "Sci-fic", 14.2f);
+        store.addDvd(dvd3);
+        store.print();
+        store.removeDVD(dvd3);
+        store.print();
+    }
+}
